184ba2a8239e110dc06c5f550b79e5e13d260c1e
Simon Støvring <mail@simonbs.dk>
2024-02-10 08:17:34 +0100

    Supports more content blocks

--- com.slack/plugin.js
+++ com.slack/plugin.js
@@ -13,36 +13,46 @@ function load() {
 }
 
 async function loadAsync() {
-  const channelIds = await getChannelIds()
+  const channelIds = await getConfiguredChannelIds()
   const messages = await getMessageInChannels(channelIds)
-  return messages.filter(e => e.type === "message").map(message => {
-    const date = new Date(parseInt(message.ts) * 1000)
-    const body = makeMessageFromBlocks(message.blocks) || message.text
-    const post = Post.createWithUriDateContent(message.permalink, date, body)
-    const host = message.permalink.split("/")[2]
-    const creatorURI = `https://${host}/team/${message.user_id}`
-    const creator = Creator.createWithUriName(creatorURI, message.user.display_name)
-    creator.avatar = message.user.image_192
-    post.creator = creator
-    if (message.files) {
-      post.attachments = message.files.slice(0, 4).map(file => {
-        const attachment = Attachment.createWithMedia(file.url_private)
-        attachment.text = file.title
-        attachment.thumbnail = file.thumb_360
-        return attachment
-      })
-    }
-    return post
-  })
+  return await Promise.all(
+    messages
+      .filter(e => e.type === "message" && e.subtype === undefined)
+      .map(async message => {
+        const date = new Date(parseInt(message.ts) * 1000)
+        const body = await stringFromBlocks(message.blocks)
+        const post = Post.createWithUriDateContent(message.permalink, date, body)
+        const host = message.permalink.split("/")[2]
+        const creatorURI = `https://${host}/team/${message.user_id}`
+        const creator = Creator.createWithUriName(creatorURI, message.user.display_name)
+        creator.avatar = message.user.image_192
+        post.creator = creator
+        if (message.files) {
+          post.attachments = message.files.slice(0, 4).map(file => {
+            const attachment = Attachment.createWithMedia(file.url_private)
+            attachment.text = file.title
+            attachment.thumbnail = file.thumb_360
+            return attachment
+          })
+        }
+        return post
+      }) 
+  )
 }
 
-async function getChannelIds() {
+/**
+ * Fetch data
+ */
+ 
+let cachedTeam = null
+let cachedUserProfiles = {}
+let cachedChannels = {}
+
+async function getConfiguredChannelIds() {
   const channelNames = channels
     .split(/,| /)
     .map(e => e.replace(/^#/, "").trim().toLowerCase())
-  const text = await sendRequest(`${site}/api/conversations.list`)
-  const obj = JSON.parse(text)
-  return obj.channels
+  return (await getChannels())
     .filter(e => channelNames.includes(e.name))
     .map(e => e.id)
 }
@@ -61,7 +71,7 @@ async function getMessageInChannel(channelId) {
   return await Promise.all(obj.messages.map(message => {
     return Promise.all([
       getPermalink(channelId, message.ts),
-      getUser(message.user),
+      getUserProfile(message.user),
     ]).then(values => {
       return {
         ...message,
@@ -80,46 +90,135 @@ async function getPermalink(channelId, ts) {
   return obj.permalink
 }
 
-async function getUser(user) {
-  const url = `${site}/api/users.profile.get?user=${user}`
+async function getUserProfile(userId) {
+  if (cachedUserProfiles[userId]) {
+    return cachedUserProfiles[userId]
+  }
+  const url = `${site}/api/users.profile.get?user=${userId}`
   const text = await sendRequest(url)
   const obj = JSON.parse(text)
+  cachedUserProfiles[userId] = obj.profile
   return obj.profile
 }
 
-function makeMessageFromBlocks(blocks) {
-  if (!blocks || blocks.length == 0 || blocks[0].type != "rich_text") {
-    return null
-  } 
-  const block = blocks[0]
-  if (!block.elements || block.elements.length == 0 || block.elements[0].type != "rich_text_section") {
-    return null
+async function getTeamInfo() {
+  if (cachedTeam) {
+    return cachedTeam
   }
-  const element = block.elements[0]
-  if (!element.elements) {
-    return null
-  }
-  var text = "<p>"
-  for (const childElement of element.elements) {
-    if (childElement.type == "text") {
-      if (childElement.style && childElement.style.bold) {
-      	text += `<strong>${childElement.text}</strong>`
-      }
-      else if (childElement.style && childElement.style.italic) {
-      	text += `<em>${childElement.text}</em>`
-      }
-      else {
-      	text += childElement.text
-      }
-    } else if (childElement.type == "emoji") {
-      text += String.fromCodePoint(parseInt(childElement.unicode, 16))
-    } else if (childElement.type == "link") {
-      text += `<a href="${childElement.url}">${childElement.url}</a>`
-    } else {
-      // TODO: A "user" type includes a user_id that needs to be retrieved and linkified.
-      console.log(`ignored childElement.type: ${childElement.type}`)
+  const url = `${site}/api/team.info`
+  const text = await sendRequest(url)
+  const obj = JSON.parse(text)
+  cachedTeam = obj.team
+  return obj.team
+}
+
+async function getChannels() {
+  const text = await sendRequest(`${site}/api/conversations.list`)
+  const obj = JSON.parse(text)
+  if (obj.channels) {
+    for (const channel of obj.channels) {
+      cachedChannels[channel.id] = channel
     }
   }
-  text += "</p>"
-  return text
+  return obj.channels
+}
+
+async function getChannel(channelId) {
+  if (cachedChannels[channelId]) {
+    return cachedChannels[channelId]
+  }
+  const url = `${site}/api/conversations.info`
+  const text = await sendRequest(url)
+  const obj = JSON.parse(text)
+  cachedChannels[channelId] = obj.channel
+  return obj.channel
+}
+
+/**
+ * Construct messages
+ */
+
+async function stringFromBlocks(blocks) {
+  if (!blocks) {
+    return ""
+  }
+  const texts = await Promise.all(
+    blocks.map(async block => {
+      if (block.type == "rich_text") {
+        return await stringFromRichTextBlock(block)
+      } else {
+        console.log(`Ignored block type: ${block.type}"`)
+        return ""
+      }
+    })
+  )
+  return texts.join("").replace(/\n/g, "<br/>")
+}
+
+async function stringFromRichTextBlock(block) {
+  const texts = await Promise.all(
+    block.elements.map(async element => {
+      if (element.type == "rich_text_section") {
+        return `<p>${await stringFromTextElements(element.elements)}</p>`
+      } else if (element.type == "rich_text_list") {
+        const items = await Promise.all(
+          element.elements.map(section => {
+            return stringFromTextElements(section.elements)
+          })
+        )
+        if (element.style == "ordered") {
+          return `<p>${items.map((e, idx) => `${idx + 1}. ${e}`).join("\n")}</p>`
+        } else {
+          return `<p>${items.map(e => `• ${e}`).join("\n")}</p>`
+        }
+      } else if (element.type == "rich_text_preformatted") {
+        const text = await stringFromTextElements(element.elements)
+        return `<p><code>${text}</code></p>`
+      } else if (element.type == "rich_text_quote") {
+        return `<p>${await stringFromTextElements(element.elements)}</p>`
+      } else {
+        console.log(`Ignored element type: ${element.type}`)
+        return ""
+      }
+    })
+  )
+  return texts.join("")
+}
+
+async function stringFromTextElements(elements) {
+  const texts = await Promise.all(elements.map(stringFromTextElement))
+  return texts.join("")
+}
+
+async function stringFromTextElement(element) {
+  if (element.type == "text") {
+    if (element.style && element.style.bold) {
+      return `<strong>${element.text}</strong>`
+    } else if (element.style && element.style.italic) {
+      return `<em>${element.text}</em>`
+    } else if (element.style && element.style.code) {
+      return `<code>${element.text}</code>`
+    } else {
+      return element.text
+    }
+  } else if (element.type == "emoji") {
+    if (element.unicode) {
+      return String.fromCodePoint(parseInt(element.unicode, 16))
+    } else {
+      return ""
+    }
+  } else if (element.type == "link") {
+    return `<a href="${element.url}">${element.url}</a>`
+  } else if (element.type == "user") {
+    const team = await getTeamInfo()
+    const user = await getUserProfile(element.user_id)
+    return `<a href="${team.url}/team/${element.user_id}">@${user.display_name}</a>`
+  } else if (element.type == "channel") {
+    const team = await getTeamInfo()
+    const channel = await getChannel(element.channel_id)
+    return `<a href="${team.url}/archives/${element.channel_id}">@${channel.name}</a>`
+  } else {
+    console.log(`Ignored element type: ${element.type}`)
+    return ""
+  }
 }
